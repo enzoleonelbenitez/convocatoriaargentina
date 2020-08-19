9a7a6291a8201dc35a93a52e67dbfcb0c4b4dced
enzoleonelbenitez <enzoleonelbenite@gmail.com>
2020-08-19 00:22:31 -0300

    Cambiando nav

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function Jugador (nombre, posicion, club) {
+this.nombre = nombre;
+this.posicion = posicion;
+this.club = club;
+}
+
+let jugador1 = new Jugador ("Esteban Andrada", "Arquero", "Boca Jrs");
+let jugador2 = new Jugador ("Juan Musso", "Arquero", "Udinese");
+let jugador3 = new Jugador ("Geronimo Rulli", "Arquero", "Real Sociedad");
+let jugador4 = new Jugador ("Franco Armani", "Arquero", "River Plate");
+let jugador5 = new Jugador ("Agustín Marchesin", "Arquero", "Club America");
+let jugador6 = new Jugador ("Emiliano Martinez", "Arquero", "Arsenal FC");
+let jugador7 = new Jugador ("Nahuel Guzman", "Arquero", "Tigres UANL");
+let jugador8 = new Jugador ("Guido Herrera", "Arquero", "Talleres Cba");
+
+
+
+
